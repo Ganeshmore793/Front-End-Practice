9db65bc11a2d8538ddde614e84d98f6500c0b11d
Ganesh More <ganumore793@gmail.com>
2025-01-28 11:44:10 +0530

    Java Script Practice

---
+++ JavaScript/Apna_college-DOM/script.js
@@ -0,0 +1,40 @@
+//Documet Object Model
+
+// let h1=document.querySelector("h1");
+
+
+
+// h1.innerText=h1.innerText+"   form apna college";
+
+// console.dir(h1.innerText);
+
+// let div=document.querySelectorAll(".box");
+
+// console.log(div);
+
+
+
+// function submit(){
+//     div[0].innerText="uniqurs 1";
+//     div[1].innerText="2dfvdfvdfv 2";    
+//         div[2].innerText="dvsdvsdv 3"; 
+        
+//         alert("You submit the text");
+// }
+
+// let newBtn=document.createElement("button");
+
+// newBtn.innerText="Click me ";
+
+// newBtn.style.backgroundColor="red";
+// newBtn.style.color="white";
+
+// document.querySelector("body").prepend(newBtn);
+
+
+let para=document.querySelector("p");
+//para.getAttribute("class")
+
+para.classList.add("newClass");
+
+
